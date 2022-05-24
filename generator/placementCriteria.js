7af6421769b7b9a51105e17f0b411d713f29e498
Filip Pitus <80021800+F-PTS@users.noreply.github.com>
2022-05-24 18:19:35 +0200

    Adding more generating functions

--- generator/placementCriteria.js
+++ generator/placementCriteria.js
@@ -9,7 +9,7 @@
 // 1. numberToPlace nie może istnieć w rzędzie
 const isRowSafe = (mapToSolve, emptyCell, numberToPlace) => {
     // jesli rząd mapy, w którym program nie znajdzie numberToPlace zwróci false
-    return mapToSolve[emptyCell.rowIndex].find(num => num == numberToPlace) == -1
+    return mapToSolve[emptyCell.rowIndex].indexOf(numberToPlace) == -1
 }
 
 // 2. numberToPlace nie może istnieć w kolumnie.
@@ -17,7 +17,7 @@ const isColumnSafe = (mapToSolve, emptyCell, numberToPlace) => {
     // żeby sprawdzić czy numberToPlace istnieje w kolumnie, trzeba
     // przeiterować przez każdy row tablicy mapToSolve (rowToCheck) i wtedy sprawdzić,
     // czy w każdym z rowToCheck[emptyCell.columnIndex] jest już numberToPlace
-    return !mapToSolve.some(rowToCheck[emptyCell.columnIndex] == numberToPlace) 
+    return !mapToSolve.some(rowToCheck => rowToCheck[emptyCell.columnIndex] == numberToPlace) 
 }
 
 // 3. numberToPlace nie może istnieć w tym samym, małym kwadracie 3x3
@@ -31,12 +31,12 @@ const isBoxSafe = (mapToSolve, emptyCell, numberToPlace) => {
     //  -------------
     //  |   |   |   |
 
-    boxStartRow = emptyCell.rowIndex - (emptyCell.rowIndex % 3) // musimy odejmować index % 3 bo js indexuje od 0
-    boxStartColumn = emptyCell.columnIndex - (emptyCell.rowIndex % 3) // musimy odejmować index % 3 bo js indexuje od 0
+    let boxStartRow = emptyCell.rowIndex - (emptyCell.rowIndex % 3) // musimy odejmować index % 3 bo js indexuje od 0
+    let boxStartColumn = emptyCell.columnIndex - (emptyCell.rowIndex % 3) // musimy odejmować index % 3 bo js indexuje od 0
     let isSafe = true
 
-    for(boxRow of [0, 1, 2]) {
-        for(boxColumn of [0, 1, 2]) {
+    for(let boxRow of [0, 1, 2]) {
+        for(let boxColumn of [0, 1, 2]) {
             if(mapToSolve[boxStartRow + boxRow][boxStartColumn + boxColumn] == numberToPlace){
                 isSafe = false
             }
@@ -50,6 +50,6 @@ const isBoxSafe = (mapToSolve, emptyCell, numberToPlace) => {
 // która nie łamie żadnych zasad
 export const safeToPlace = (mapToSolve, emptyCell, numberToPlace) => {
     return isRowSafe(mapToSolve, emptyCell, numberToPlace) &&
-            isColumnSafe(mapToSolve, emptyCell, numberToPlace) &&
-            isBoxSafe(mapToSolve, emptyCell, numberToPlace)
+    isColumnSafe(mapToSolve, emptyCell, numberToPlace) &&
+    isBoxSafe(mapToSolve, emptyCell, numberToPlace)
 }
--- generator/sudokuGenerator.js
+++ generator/sudokuGenerator.js
@@ -1,6 +1,6 @@
 import { safeToPlace } from "./placementCriteria.js";
 
-const BLANK_BOARD = [
+const blankMap = [
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -14,26 +14,73 @@ const BLANK_BOARD = [
 
 // żeby wypełnić randomowymi wartościami (funkcja fillSudoku), najpierw musimy sprawić 
 // czy wszystkie komórki z wartością 0 zostały wskazane przez funkcję nextEmptyCell.
-// następnie musimy wylosować random liczbę sprzedziału (0, 9>
-// czyli w naszym wypadku posibbleNumbers
+// następnie musimy wylosować liczbę sprzedziału (0, 9> czyli posibbleNumbers
 
-const posibbleNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+// najpierw musimy przetasować possibleNUmbers
+const possibleNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+const shuffle = array => {
+    let sortedArray = [...array] // klonujemy possibleNumbers, by wykonywać operacje na nowej tablicy.
+
+    for(let i = sortedArray.length - 1; i > 0; i--){
+        const j = Math.floor(Math.random() * (i + 1));
+        [ sortedArray[i], sortedArray[j] ] = [ sortedArray[j], sortedArray[i] ]; // przypisanie wielu zmiennych na raz
+    }
+
+    return sortedArray;
+}
 
 const nextEmptyCell = mapToSolve => {
     const emptyCell = {rowIndex: "", columnIndex: ""}
 
     mapToSolve.forEach( (row, rowIndex) => {
-        if(emptyCell.columnIndex !== "") return
+        if(emptyCell.columnIndex !== "") return;
         
-        let firstZero = row.find(column => column === 0)
+        let firstZero = row.find(column => column == 0)
     
         //jeśli nie ma więcej zer
-        if(!firstZero) return
+        if(firstZero === undefined) return;
         emptyCell.rowIndex = rowIndex
         emptyCell.columnIndex = row.indexOf(firstZero)
     })
 
     if (emptyCell.columnIndex !== "") return emptyCell
-    // jeśli emptycell jest puste to znaczy że nie ma już żadnych zer
+
+    // jeśli emptyCell jest puste to znaczy że nie ma już żadnych zer
     return false
 }
+
+const fillSudoku = mapToFill => {
+    const emptyCell = nextEmptyCell(mapToFill);
+
+    console.log('jdvaudv ', emptyCell)
+
+    if (!emptyCell) return mapToFill
+
+    for(let numberToPlace of shuffle(possibleNumbers)){
+        if (safeToPlace(mapToFill, emptyCell, numberToPlace)){
+            // wstaw numberToPlace
+            mapToFill[emptyCell.rowIndex][emptyCell.columnIndex] = numberToPlace
+        
+            // rekurencja funkcji, żeby wypełnić pozostałę emptyCell w mapToFill
+            if(fillSudoku(mapToFill)) return mapToFill
+    
+            // jeśli nie da się wypełnić następnego pola numberToPlace, to numberToPlace ma złą wartość
+            // dzięki algorytmowi backtrackingowmu resetujemy całą akcję i próbujeny z następnym numerem
+            mapToFill[emptyCell.rowIndex][emptyCell.columnIndex] = 0
+        }
+    }
+
+    // kolejny element backtrackingu, gdy nie możemy wpisać żadnego numeru zwracamy fałsz,
+    // co sprawia, że poprzedni cykl wstawiania się resetuje i zaczynamy następny
+    return false
+}
+
+const newSolvedMap = () => {
+    const newMap = blankMap.map( row => row.slice() ) // nie użyłem [...blankMap] bo słabo nadaje się to do klonowania wielowymiarowych tablic.
+    const filledMap = fillSudoku(newMap)
+    return filledMap
+}
+
+console.log(newSolvedMap());
+
